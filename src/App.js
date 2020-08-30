55cea3e99cb5d3fcbce971a4d14169ff137c7c1a
kjuiop <copying_y@naver.com>
2020-08-30 22:39:28 +0900

    2048 game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Header from './component/Header';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
+import useLocalStorageNumber from './hook/useLocalStorageNumber';
 
 export default function App() {
-  return (<div className="App"></div>);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber('bestScore', 0);
+
+  useEffect(() => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+  });
+
+  return (
+    <div className="container">
+      <Header score={score} bestScore={bestScore} />
+      <AboveGame />
+      <Game setScore={setScore} />
+    </div>
+  );
 }
